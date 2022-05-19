00be40a212802ab951113a0a2f3f00116badd1f6
Anton.Darnostup <antondarnostup1990@gmail.com>
2022-05-19 21:58:34 +0300

    two

---
+++ src/Class.java
@@ -0,0 +1,15 @@
+public class Class {
+
+    //| a-b | /(a+b)^3 - √c
+    static int a = 32;
+    static int b = 12;
+    static int c = 8;
+
+    public static void main(String[] args)
+    {
+        int с = 4;
+        System.out.println(  a-b  /(a+b)^3 - с);
+    }
+
+
+}
